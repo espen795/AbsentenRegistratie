89c73ee8aebe826aebdc09e25bb6a02ced412a05
Bas Tolen <bastolen97@gmail.com>
2018-05-23 19:45:42 +0200

    classes aangemaakt

---
+++ AbsentenRegistratie/Models/Professor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AbsentenRegistratie.Models;
+
+namespace AbsentenRegistratie.Models
+{
+    public class Professor
+    {
+        private int id;
+        private string name;
+        private string email;
+        private string password;
+        public List<Subject> subjects;
+
+    }
+}
---
+++ AbsentenRegistratie/Models/Subject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AbsentenRegistratie.Models
+{
+    public class Subject
+    {
+        private int id;
+        private string name;
+    }
+}
